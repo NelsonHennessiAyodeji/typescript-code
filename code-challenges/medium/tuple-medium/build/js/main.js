8798d39ec54a9c39877f386a8105b378b8f70bdb
Ayodeji Nelson <nelsonhennessiayodeji@gmail.com>
2026-07-02 17:41:46 +0100

    Completed Tuple Medium Challenge

---
+++ code-challenges/medium/tuple-medium/build/js/main.js
@@ -0,0 +1,10 @@
+const employess = [[101, "David", 22000, "HR"], [102, "Tayo", 5000, "Tech"],
+    [103, "Mary", 6700, "Law"], [104, "Hume", 11000, "HR"], [105, "Sarah", 14000, "Egnineering"]];
+// Log employess earning above 10,000
+for (let i = 0; i < employess.length; i++) {
+    if ((employess[i]?.[2] ?? 0) > 10000) {
+        console.log(employess[i]);
+    }
+}
+export {};
+//# sourceMappingURL=main.js.map
