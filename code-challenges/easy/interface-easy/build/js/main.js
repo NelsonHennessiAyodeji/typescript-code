a6cb9a25bdd07d5c448acaddc0421c8a075828af
Ayodeji Nelson <nelsonhennessiayodeji@gmail.com>
2026-07-02 18:46:18 +0100

    Completed Interface Easy

---
+++ code-challenges/easy/interface-easy/build/js/main.js
@@ -0,0 +1,26 @@
+const landa = {
+    name: "Landa",
+    breed: "Rott",
+    age: 3,
+    vaccinated: true
+};
+const chyna = {
+    name: "Chyna",
+    breed: "Golden Retriever",
+    age: 5,
+    vaccinated: false
+};
+const rob = {
+    name: "Rob",
+    breed: "Golden Retriever",
+    age: 3,
+    vaccinated: true
+};
+const jack = {
+    name: "Jack",
+    breed: "Rott",
+    age: 3,
+    vaccinated: true
+};
+export {};
+//# sourceMappingURL=main.js.map
---
+++ code-challenges/easy/interface-easy/src/main.ts
@@ -0,0 +1,34 @@
+interface Dog {
+    name: string;
+    breed: string;
+    age: number;
+    vaccinated: boolean;
+}
+
+const landa: Dog = {
+    name: "Landa",
+    breed:"Rott",
+    age: 3,
+    vaccinated: true
+}
+
+const chyna: Dog = {
+    name: "Chyna",
+    breed:"Golden Retriever",
+    age: 5,
+    vaccinated: false
+}
+
+const rob: Dog = {
+    name: "Rob",
+    breed:"Golden Retriever",
+    age: 3,
+    vaccinated: true
+}
+
+const jack: Dog = {
+    name: "Jack",
+    breed:"Rott",
+    age: 3,
+    vaccinated: true
+}
