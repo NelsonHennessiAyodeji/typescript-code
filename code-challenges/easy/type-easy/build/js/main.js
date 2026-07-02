f148cab2968692a9489bf976206eae69136c572e
Ayodeji Nelson <nelsonhennessiayodeji@gmail.com>
2026-07-02 18:11:22 +0100

    Completed Type Easy

---
+++ code-challenges/easy/type-easy/build/js/main.js
@@ -0,0 +1,21 @@
+// Create 3 cars
+const Toyota = {
+    brand: "Toyota",
+    model: "208yh",
+    year: 2012,
+    electric: false
+};
+const Camry = {
+    brand: "Camry",
+    model: "97fg",
+    year: 2014,
+    electric: false
+};
+const Tesla = {
+    brand: "Tesla",
+    model: "3000T",
+    year: 2021,
+    electric: true
+};
+export {};
+//# sourceMappingURL=main.js.map
---
+++ code-challenges/easy/type-easy/src/main.ts
@@ -0,0 +1,28 @@
+type Car = {
+    brand: string;
+    model: string;
+    year: number;
+    electric: boolean;
+}
+
+// Create 3 cars
+const Toyota: Car = {
+    brand: "Toyota",
+    model: "208yh",
+    year: 2012,
+    electric: false
+}
+
+const Camry: Car = {
+    brand: "Camry",
+    model: "97fg",
+    year: 2014,
+    electric: false
+}
+
+const Tesla: Car = {
+    brand: "Tesla",
+    model: "3000T",
+    year: 2021,
+    electric: true
+}
