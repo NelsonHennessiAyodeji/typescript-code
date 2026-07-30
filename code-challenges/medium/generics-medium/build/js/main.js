5f8f5b01055c080a240020a1fda64827a2fc71d4
Ayodeji Nelson <nelsonhennessiayodeji@gmail.com>
2026-07-30 16:22:54 +0100

    Completed Generics-Medium

--- code-challenges/medium/generics-medium/build/js/main.js
+++ code-challenges/medium/generics-medium/build/js/main.js
@@ -27,22 +27,100 @@ const userRepository = new Repository([
     { id: 5, name: "David", age: 31 },
 ]);
 const employeeRepository = new Repository([
-    { name: "Tyler", salary: 2000 },
-    { name: "Grace", salary: 3500 },
-    { name: "Michael", salary: 4200 },
-    { name: "Sophia", salary: 3900 },
-    { name: "Daniel", salary: 5100 },
+    { id: 1, name: "Tyler", salary: 2000 },
+    { id: 2, name: "Grace", salary: 3500 },
+    { id: 3, name: "Michael", salary: 4200 },
+    { id: 4, name: "Sophia", salary: 3900 },
+    { id: 5, name: "Daniel", salary: 5100 },
 ]);
 const bookRepository = new Repository([
-    { title: "Harry Potter", author: "J.K. Rowling" },
-    { title: "The Hobbit", author: "J.R.R. Tolkien" },
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
-    { title: "1984", author: "George Orwell" },
-    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
+    { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { id: 4, title: "1984", author: "George Orwell" },
+    { id: 5, title: "To Kill a Mockingbird", author: "Harper Lee" },
 ]);
-userRepository.remove("id", 2);
-userRepository.getAll();
-employeeRepository.getAll();
-bookRepository.getAll();
+// userRepository.remove("id", 2);
+// userRepository.getAll();
+// employeeRepository.getAll();
+// bookRepository.getAll();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Generic Search Utility
+let users = [
+    { id: 1, name: "Chris", age: 20 },
+    { id: 2, name: "Sarah", age: 24 },
+    { id: 3, name: "John", age: 19 },
+    { id: 4, name: "Emily", age: 27 },
+    { id: 5, name: "David", age: 31 },
+];
+let employees = [
+    { id: 1, name: "Tyler", salary: 2000 },
+    { id: 2, name: "Grace", salary: 3500 },
+    { id: 3, name: "Michael", salary: 4200 },
+    { id: 4, name: "Sophia", salary: 3900 },
+    { id: 5, name: "Daniel", salary: 5100 },
+];
+let books = [
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
+    { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { id: 4, title: "1984", author: "George Orwell" },
+    { id: 5, title: "To Kill a Mockingbird", author: "Harper Lee" },
+];
+const searchById = (array, id) => {
+    const item = array[(id - 1)];
+    if (item) {
+        return item;
+    }
+    return undefined;
+};
+const loginForm = {
+    email: "john@gmail.com",
+    password: 123456,
+};
+const registerForm = {
+    firstName: "John",
+    lastName: "Smilga",
+    email: "john@gmail.com",
+    password: 123456,
+    confirmPassword: 123456,
+};
+const paymentForm = {
+    cardNumber: 2315416578,
+    cardHolder: "John Smilga",
+    amount: 20000,
+    cvv: 220,
+};
+const loginValidation = {
+    valid: true,
+    errors: ["Success"],
+    data: loginForm,
+};
+const registerValidation = {
+    valid: false,
+    errors: ["Failed"],
+    data: registerForm,
+};
+const paymentValidation = {
+    valid: false,
+    errors: ["CVV must be 3 digits."],
+    data: paymentForm,
+};
+const printValidationResult = (result) => {
+    const isValid = result.valid;
+    const errors = result.errors;
+    if (isValid) {
+        console.log("Validation Successful");
+    }
+    else {
+        for (let i = 0; i < errors.length; i++) {
+            console.log(errors[i]);
+        }
+    }
+    console.log(result.data);
+};
+printValidationResult(loginValidation);
+printValidationResult(registerValidation);
+printValidationResult(paymentValidation);
 export {};
 //# sourceMappingURL=main.js.map
--- code-challenges/medium/generics-medium/src/main.ts
+++ code-challenges/medium/generics-medium/src/main.ts
@@ -29,11 +29,13 @@ type User = {
 }
 
 type Employee = {
+    id: number,
     name: string,
     salary: number,
 }
 
 type Book = {
+    id: number,
     title: string,
     author: string,
 }
@@ -47,23 +49,148 @@ const userRepository = new Repository<User>([
 ]);
 
 const employeeRepository = new Repository<Employee>([
-    { name: "Tyler", salary: 2000 },
-    { name: "Grace", salary: 3500 },
-    { name: "Michael", salary: 4200 },
-    { name: "Sophia", salary: 3900 },
-    { name: "Daniel", salary: 5100 },
+    { id: 1, name: "Tyler", salary: 2000 },
+    { id: 2, name: "Grace", salary: 3500 },
+    { id: 3, name: "Michael", salary: 4200 },
+    { id: 4, name: "Sophia", salary: 3900 },
+    { id: 5, name: "Daniel", salary: 5100 },
 ]);
 
 const bookRepository = new Repository<Book>([
-    { title: "Harry Potter", author: "J.K. Rowling" },
-    { title: "The Hobbit", author: "J.R.R. Tolkien" },
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
-    { title: "1984", author: "George Orwell" },
-    { title: "To Kill a Mockingbird", author: "Harper Lee" },
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
+    { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { id: 4, title: "1984", author: "George Orwell" },
+    { id: 5, title: "To Kill a Mockingbird", author: "Harper Lee" },
 ]);
 
-userRepository.remove("id", 2);
-userRepository.getAll();
-employeeRepository.getAll();
-bookRepository.getAll();
+// userRepository.remove("id", 2);
+// userRepository.getAll();
+// employeeRepository.getAll();
+// bookRepository.getAll();
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Generic Search Utility
+let users: User[] = [    
+    { id: 1, name: "Chris", age: 20 },
+    { id: 2, name: "Sarah", age: 24 },
+    { id: 3, name: "John", age: 19 },
+    { id: 4, name: "Emily", age: 27 },
+    { id: 5, name: "David", age: 31 },
+];
+
+let employees: Employee[] = [
+    { id: 1, name: "Tyler", salary: 2000 },
+    { id: 2, name: "Grace", salary: 3500 },
+    { id: 3, name: "Michael", salary: 4200 },
+    { id: 4, name: "Sophia", salary: 3900 },
+    { id: 5, name: "Daniel", salary: 5100 },
+];
+
+let books: Book[] = [
+    { id: 1, title: "Harry Potter", author: "J.K. Rowling" },
+    { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien" },
+    { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
+    { id: 4, title: "1984", author: "George Orwell" },
+    { id: 5, title: "To Kill a Mockingbird", author: "Harper Lee" },
+];
+
+const searchById = <T>(array: T[], id: number): (T | undefined | any) => {
+    const item = array[(id - 1) as keyof T[]];
+    if (item) {
+        return item;
+    } 
+    return undefined;
+}
+
+// console.log(searchById<User>(users, 3));
+// console.log(searchById(employees, 5));
+// console.log(searchById<Book>(books, 2));
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Generic Form Validator
+interface ValidationResult<T> {
+    valid: boolean;
+    errors: string[];
+    data: T
+}
+
+type LoginForm = {
+    email: string, 
+    password: number
+}
+
+type RegisterForm = {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: number
+    confirmPassword: number
+}
+
+type PaymentForm = {
+    cardNumber: number,
+    cardHolder: string,
+    amount: number,
+    cvv: Number,
+}
+
+const loginForm: LoginForm = {
+    email: "john@gmail.com",
+    password: 123456,
+}
+
+const registerForm: RegisterForm = {
+    firstName: "John",
+    lastName: "Smilga",
+    email: "john@gmail.com",
+    password: 123456,
+    confirmPassword: 123456,
+}
+
+const paymentForm: PaymentForm = {
+    cardNumber: 2315416578,
+    cardHolder: "John Smilga",
+    amount: 20000,
+    cvv: 220,
+}
+
+const loginValidation: ValidationResult<LoginForm> = {
+    valid: true,
+    errors: ["Success"],
+    data: loginForm,
+}
+
+const registerValidation: ValidationResult<RegisterForm> = {
+    valid: false,
+    errors: ["Failed"],
+    data: registerForm,
+}
+
+const paymentValidation: ValidationResult<PaymentForm> = {
+    valid: false,
+    errors: ["CVV must be 3 digits."],
+    data: paymentForm,
+}
+
+const printValidationResult = <T>(result: ValidationResult<T>): void => {
+    const isValid: boolean = result.valid;
+    const errors: string[] = result.errors;
+
+    if (isValid) {
+        console.log("Validation Successful");
+    } else {
+        for (let i = 0; i < errors.length; i++) {
+            console.log(errors[i]);
+        }
+    }
+    console.log(result.data);
+}
+
+printValidationResult(loginValidation)
+
+printValidationResult<RegisterForm>(registerValidation)
+
+printValidationResult(paymentValidation)
