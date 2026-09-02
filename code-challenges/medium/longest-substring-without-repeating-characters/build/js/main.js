ef81f0754f0a875f36d2ccf69792a5ac3f24c4b6
Ayodeji Nelson <nelsonhennessiayodeji@gmail.com>
2026-09-02 17:10:21 +0100

    Completed Longest Substring Challenge

--- code-challenges/medium/longest-substring-without-repeating-characters/build/js/main.js
+++ code-challenges/medium/longest-substring-without-repeating-characters/build/js/main.js
@@ -1,3 +1,15 @@
-console.log("JI");
+const s = "abcabcbb";
+function longestSubstring(s) {
+    let longestSubstring = 0;
+    let stringArr = [];
+    for (let i = 0; i < s.length; i++) {
+        if (stringArr.includes(s.charAt(i)))
+            stringArr = [];
+        stringArr.push(s.charAt(i));
+        longestSubstring = (longestSubstring < stringArr.length) ? stringArr.length : longestSubstring;
+    }
+    return longestSubstring;
+}
+console.log(longestSubstring(s));
 export {};
 //# sourceMappingURL=main.js.map
--- code-challenges/medium/longest-substring-without-repeating-characters/src/main.ts
+++ code-challenges/medium/longest-substring-without-repeating-characters/src/main.ts
@@ -1,1 +1,15 @@
-console.log("JI");
+const s = "abcabcbb";
+
+function longestSubstring(s: string) {
+    let longestSubstring: number = 0;
+    let stringArr: string[] = [];
+    for (let i: number = 0; i < s.length; i++) {
+        if (stringArr.includes(s.charAt(i))) stringArr = [];
+        stringArr.push(s.charAt(i));
+        longestSubstring = (longestSubstring < stringArr.length)? stringArr.length : longestSubstring;
+    }
+
+    return longestSubstring;
+}
+
+console.log(longestSubstring(s));
